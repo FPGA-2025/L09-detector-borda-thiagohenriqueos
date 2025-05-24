9cf2973203430a4100167998e123e71ce6cdcb1d
thiagohenriqueos <henrq_11@hotmail.com>
2025-05-24 17:54:43 -0300

    Corrige permissão de execução dos scripts

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
+#!/bin/sh
+
 if [ -z "$1" ]; then
     echo "Erro: É preciso especificar o número do teste"
     exit 1
 fi
+
 iverilog -o tb *.v
 rm -f saida.out
 cp test/teste$1.txt teste.txt
 ./tb > saida.out
 cp saida.out test/saida$1.out
 cp saida.vcd test/saida$1.vcd
-rm saida.out saida.vcd teste.txt
 
-if diff <(grep -v '\$finish' test/saida$1.out) test/saida$1.ok >/dev/null; then
+# Evita uso de <( ) substituindo com arquivo temporário
+grep -v '\$finish' test/saida$1.out > tmp_saida_filtrada.out
+if diff tmp_saida_filtrada.out test/saida$1.ok >/dev/null; then
     echo "OK"
-    exit 0
+    result=0
 else
     echo "ERRO"
-    exit 1
+    result=1
 fi
 
+rm saida.out saida.vcd teste.txt tmp_saida_filtrada.out
+exit $result
