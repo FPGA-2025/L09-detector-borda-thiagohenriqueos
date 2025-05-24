eacc41d2e5fa91e1a39e341e059cb578452437ab
thiagohenriqueos <henrq_11@hotmail.com>
2025-05-24 17:28:43 -0300

    Todos os testes Ok

--- detector-borda.v
+++ detector-borda.v
@@ -2,9 +2,22 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
-    
-//insira seu código aqui
+
+    reg [1:0] entrada_antiga; // Guarda o valor anterior da entrada para comparação
+
+    always @(posedge clk or rst) begin
+        if (rst) begin
+            entrada_antiga <= 2'b00;
+            detector <= 2'b00;
+        end else begin
+            // Detecta borda de subida: antes era 0 e agora é 1
+            detector <= (~entrada_antiga) & entrada;
+
+            // Atualiza o valor antigo com o valor atual
+            entrada_antiga <= entrada;
+        end
+    end
 
 endmodule
